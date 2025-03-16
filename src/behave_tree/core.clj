e4bf8329f2628fe83e5ee8b52c92b42abed691a1
Peter McTaggart <pmctaggart@powercor.com.au>
2025-03-17 08:29:47 +1100

    working version

--- src/behave_tree/core.clj
+++ src/behave_tree/core.clj
@@ -3,7 +3,8 @@
    [aido.compile :as ac]
    [aido.core :as ai]
    [aido.tick :as at]
-   [java-time.api :as time]))
+   [java-time.api :as time]
+   [clojure.pprint :as pp]))
 
 
 (defn in-interval?
@@ -12,11 +13,11 @@
   (and (>= (.getHour time) start-hour)
        (< (.getHour time) end-hour)))
 
-(defn in-free-charge-time? [time]
-  (in-interval? time 11 13))
+(defn in-free-charge-time? [t]
+  (in-interval? t 11 13))
 
-(defn in-cheap-charge-time? [time]
-  (in-interval? time 0 6))
+(defn in-cheap-charge-time? [t]
+  (in-interval? t 0 6))
 
 (defn minutes-until-target [current-time target-time-of-day]
   (let [target-time (time/local-time target-time-of-day)
@@ -42,43 +43,50 @@
 
 ;; aido behaviors for the battery
 (defmethod at/tick :battery-charged?
-  [db]
+  [db & children]
+  (println "Checking battery charged")
   (if (>= (:soc (:state db)) 100)
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :battery-soc-80?
-  [db]
+  [db & children]
+  (println "Checking battery soc 80")
   (if (>= (:soc (:state db)) 80)
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :battery-soc-20?
-  [db]
+  [db & children]
+  (println "Checking battery soc 20")
   (if (<= (:soc (:state db)) 20)
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :free-charge-time?
-  [db]
+  [db & children]
+  (println "Checking free charge time")
   (if (in-free-charge-time? (time/local-time))
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :cheap-charge-time?
-  [db]
+  [db & children]
+  (println "Checking cheap charge time")
   (if (in-cheap-charge-time? (time/local-time))
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :car-charging?
-  [db]
+  [db & children]
+  (println "Checking car charging")
   (if (:car-charging? (:state db))
     (ai/tick-success db)
     (ai/tick-failure db)))
 
 (defmethod at/tick :forecast-soc-45-at-6am?
-  [db]
+  [db & children]
+  (println "Checking forecast soc 45 at 6am")
   (let [current-time (time/local-time)]
     (if (forecast-soc? (:soc (:state db))
                        45
@@ -89,16 +97,19 @@
       (ai/tick-failure db))))
 
 (defmethod at/tick :charge-battery
-  [db]
+  [db & children]
+  (println "Charging battery")
   (ai/tick-success (assoc db :state {:soc 100})))
 
 (defmethod at/tick :wait-1-minute
-  [db]
+  [db & children]
+  (println "Waiting 1 minute")
   (do (Thread/sleep 60000)
       (ai/tick-success db)))
 
 (defmethod at/tick :nothing-to-do
-  [db]
+  [db & children]
+  (println "Nothing to do")
   (ai/tick-success db))
 
 (def battery-behaviour-tree
@@ -110,8 +121,8 @@
       [:sequence
        [:free-charge-time?]
        [:selector
-        [:car-charging?
-         :charge-battery]]]
+        [:car-charging?] 
+        [:charge-battery]]]
       [:sequence
        [:cheap-charge-time?]
        [:selector
@@ -122,17 +133,19 @@
      [:nothing-to-do]]
     [:wait-1-minute]]])
 
-(defn- main
+(defn main
   ;; Running the behavior tree
   []
+  (print "Starting...")
   (let [fns {}
-        _ (println "Tree compiling...")
+        _ (print "Tree compiling...")
         tree (ac/compile battery-behaviour-tree fns)
-        _ (println (str "Tree compiled successfully" tree))
+        _ (println  (str "Tree compiled successfully:" (count tree)))
+        _ (pp/pprint tree)
         db {:state {:soc 80
-                    :car-charging? false}}]
-    (let [{:keys [db status]} (ai/run-tick db tree)]
+                    :car-charging? false}}
+        {:keys [db status]} (ai/run-tick db tree)]
       (print (str "db:" db))
-      (if (ai/SUCCESS status)
+      (if (= ai/SUCCESS status)
         (print "Tree finished successfully")
-        (print "Tree finished with failure")))))
+        (print "Tree finished with failure"))))
