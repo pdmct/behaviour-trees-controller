f2e75995672f6d3c09f3eee6425e69af7548bdfb
Peter McT <peter.mctaggart@gmail.com>
2025-03-17 15:59:15 +1100

    Add function to send text message using Twilio API
    Add functionality to send text messages using the Twilio API.
    
    * Add `send-text-message` function in `src/behave_tree/twilio.clj` to send a text message using the Twilio API.
    * Add Twilio configuration (account SID, auth token, from number) to `resources/config.edn`.
    * Add test for `send-text-message` function in `test/behave_tree/twilio_test.clj`.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/pdmct/behaviour-trees-example?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ test/behave_tree/twilio_test.clj
@@ -0,0 +1,21 @@
+(ns behave-tree.twilio-test
+  (:require [clojure.test :refer :all]
+            [behave-tree.twilio :as twilio]
+            [clj-http.client :as client]))
+
+(deftest test-send-text-message
+  (let [dummy-response {:body {:status "queued"}}
+        dummy-to-number "+1234567890"
+        dummy-message "Test message"]
+    (with-redefs [client/post (fn [url opts]
+                                (is (= "https://api.twilio.com/2010-04-01/Accounts/your_account_sid/Messages.json" url))
+                                (is (= {:basic-auth ["your_account_sid" "your_auth_token"]
+                                        :form-params {:from "your_twilio_number"
+                                                      :to dummy-to-number
+                                                      :body dummy-message}
+                                        :as :json} opts))
+                                dummy-response)]
+      (is (= {:status "queued"} (twilio/send-text-message dummy-to-number dummy-message))))))
+
+(comment
+  (clojure.test/run-tests))
